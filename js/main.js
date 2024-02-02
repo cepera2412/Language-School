e23d39276383e9db30c80c07d7fb0701c137f876
Serhii Demediuk <s.demeduk@gmail.com>
2024-02-02 12:34:38 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Menu mobile
+function toggleDropdown() {
+    var dropdownContent = document.querySelector('.disp');
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+}
